9d9cfad132867097231ced8d82040fcdc7684186
mesud07 <56825677+mesud07@users.noreply.github.com>
2022-02-17 02:35:03 +0300

    fonksiyon aşağıya alındı

--- lib/feature/splash/view/splash_view.dart
+++ lib/feature/splash/view/splash_view.dart
@@ -22,11 +22,38 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
 
-    
     super.initState();
     bekletVeYonlendir();
   }
-  void bekletVeYonlendir()async{
+ 
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: _buildSplashColumn(context),
+    );
+  }
+
+  Widget _buildSplashColumn(BuildContext context) {
+    return Center(
+      child: AnimatedContainer(
+       width: isbig==0?context.width * 0.5:isbig==1?context.width*0.7:isbig==2?context.width*0.6:isbig==3?context.width*0.9:context.width*0.8,
+          height: isbig==0?context.height * 0.3:isbig==1?context.height*0.5:isbig==2?context.height*0.4:isbig==3?context.height*0.6:context.height*0.5,
+       //color: Colors.white,
+        curve: Curves.easeInBack,
+        duration: Duration(seconds: 1),
+        child: Image.asset(SplashImage.splashImage)
+      ),
+    );
+  }
+
+
+
+
+
+   void bekletVeYonlendir()async{
 
 
         await Future.delayed(Duration(seconds: 1),(){
@@ -72,28 +99,6 @@ class _SplashScreenState extends State<SplashScreen> {
 
     }
 
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: _buildSplashColumn(context),
-    );
-  }
-
-  Widget _buildSplashColumn(BuildContext context) {
-    return Center(
-      child: AnimatedContainer(
-       width: isbig==0?context.width * 0.5:isbig==1?context.width*0.7:isbig==2?context.width*0.6:isbig==3?context.width*0.9:context.width*0.8,
-          height: isbig==0?context.height * 0.3:isbig==1?context.height*0.5:isbig==2?context.height*0.4:isbig==3?context.height*0.6:context.height*0.5,
-       //color: Colors.white,
-        curve: Curves.easeInBack,
-        duration: Duration(seconds: 1),
-        child: Image.asset(SplashImage.splashImage)
-      ),
-    );
-  }
 }
 
 
